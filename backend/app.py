2672533a6cb93a93d94195ad08f638ee36b59b97
Pavel_Glazunov <p6282813@yandex.ru>
2023-08-08 14:21:36 +0300

    flask app

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    application.run("127.0.0.1", 8080, debug=True)
